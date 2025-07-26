f9d8b3765cdd581eb626c2314422fee99d2f2355
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2025-07-26 21:24:57 +0300

    сделала запрос

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,13 @@
 // import css from "./App.module.css";
 import SearchBar from "../SearchBar/SearchBar";
-import { useState } from "react";
+
 
 import { fetchMovies } from "../../services/movieService";
 export default function App() {
-  const [movies, setMovies] = useState<Movie[]>([]);
+ 
   const handlerSearchForm = async (data: string) => {
     const moviesRequestResult = await fetchMovies(data);
-    setMovies(moviesRequestResult);
+    console.log(moviesRequestResult);
   };
 
   return <SearchBar onSubmit={handlerSearchForm} />;
