571a9cf222f9a63dcbdec4c5f30389c636943681
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2025-07-26 23:03:58 +0300

    переписала три компонента с отображением списка фильмов с использованием useQuery

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,32 @@
 // import css from "./App.module.css";
 import SearchBar from "../SearchBar/SearchBar";
-
-
+import { useQuery } from "@tanstack/react-query";
+import { useState, useEffect } from "react";
 import { fetchMovies } from "../../services/movieService";
-export default function App() {
- 
-  const handlerSearchForm = async (data: string) => {
-    const moviesRequestResult = await fetchMovies(data);
-    console.log(moviesRequestResult);
-  };
+import type { Movie } from "../../types/movie";
+import MovieGrid from "../MovieGrid/MovieGrid";
 
-  return <SearchBar onSubmit={handlerSearchForm} />;
+export default function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [query, setQuery] = useState("");
+
+  const { data } = useQuery({
+    queryKey: ["movie", query],
+    queryFn: () => fetchMovies(query),
+    enabled: query !== "",
+  });
+  const handlerSearchForm = async (newQuery: string) => {
+    setQuery(newQuery);
+  };
+ useEffect(() => {
+    if (data) {
+      setMovies(data);
+    }
+  }, [data]);
+  return (
+    <>
+      <SearchBar onSubmit={handlerSearchForm} />
+      {movies.length > 0 && <MovieGrid movies={movies} />}
+    </>
+  );
 }
